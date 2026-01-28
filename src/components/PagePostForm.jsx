f9331382521afc3dd29548d756091b20e91c43e6
alexander guzman <Alex.networking93@gmail.com>
2026-01-28 17:35:48 +0100

    add initial state for post form

---
+++ src/components/PagePostForm.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+
+function PagePostForm() {
+  // Stato del form : rappresenta il post in fase di compilazione
+  const [post, setPost] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: false
+  })
+
+
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlfor="exampleInputEmail1" className="form-label">Autore</label>
+        <input onChange={AddData} name="author" value={post.author} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+      </div>
+      <div className="mb-3">
+        <label htmlfor="exampleInputPassword1" className="form-label">Titolo</label>
+        <input onChange={AddData} name="title" value={post.title} type="text" className="form-control" id="exampleInputPassword1" />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">Descrizione</label>
+        <textarea onChange={AddData} name="body" value={post.body} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+      <div className="mb-3 form-check">
+        <input onChange={AddData} name="public" type="checkbox" checked={post.public} className="form-check-input" id="exampleCheck1" />
+        <label className="form-check-label" htmlFor="exampleCheck1">Public</label>
+      </div>
+
+
+    </form>
+
+
+  )
+}
+
+
+export default PagePostForm
